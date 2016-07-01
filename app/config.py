402a2967753b76caa0bdd4d6cdc8de1f17fc25c4
Andrew Kamau <andkamau@gmail.com>
2016-07-01 20:45:23 +0300

    add data from twitter

--- app/config.py
+++ app/config.py
@@ -3,11 +3,16 @@ redis_host = os.getenv('REDIS_HOST', 'localhost:6379')
 REDIS = dict(
         host=redis_host.split(':')[0],
         port=redis_host.split(':')[1],
-        db='0',
+        db='4',
         password=os.getenv('REDIS_PASSWORD', None),
         socket_timeout=2,
         socket_connect_timeout=2,
         )
+
+TWITTER_HANDLE = '@carolinespencer'
+TWITTER_STATUS_ID = '748567826907136000'
+DEFAULT_PHOTO = "https://d30y9cdsu7xlg0.cloudfront.net/png/370596-200.png"
+PORT = 8040
 
 
 tw_creds = os.getenv('TWITTER_API_CREDENTIALS').split(',')
---
+++ app/run.py
@@ -0,0 +1,17 @@
+import time
+from app import Twitter
+
+if __name__ == '__main__':
+    tw = Twitter()
+    counter = 0
+    _max = None
+    while counter < 1000:
+        max_id, min_id = tw.fetch_all(maxid=_max)
+
+        max_status = tw.api.get_status(max_id)
+        min_status = tw.api.get_status(min_id)
+        print "%s - fetched all from [%s | %s] to [%s | %s]" % (
+                counter, max_id, max_status.created_at, min_id, min_status.created_at)
+        _max = min_id
+        counter += 1
+        time.sleep(5)
